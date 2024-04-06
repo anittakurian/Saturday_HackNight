5350f5c6a7c1c04ae7d65ce3c446074130e9e21f
Sravan Chand <101858035+ogfourtwo@users.noreply.github.com>
2024-04-06 23:28:22 +0530

    final commit yaay

--- food/core/urls.py
+++ food/core/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('game',views.game, name='game'),
     path('game2',views.game2, name='game2'),
     path('game3',views.game3, name='game3'),
+    path('game4',views.game4, name='game4'),
+    path('game5',views.game5, name='game5'),
+    path('game6',views.game6, name='game6'),
+    path('game6m',views.game6m, name='game6m'),
 ]
--- food/core/views.py
+++ food/core/views.py
@@ -13,3 +13,15 @@ def game2(request):
 
 def game3(request):
     return render(request, 'game3.html')
+
+def game4(request):
+    return render(request, 'game4.html')
+
+def game5(request):
+    return render(request, 'game5.html')
+    
+def game6(request):
+    return render(request, 'game6.html')
+
+def game6m(request):
+    return render(request, 'game6m.html')
